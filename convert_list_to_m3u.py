f629a7edcba72b6ce054a4cb33725ed1accbe013
linshixiong <linsx.lin@emdoor.com>
2023-10-10 10:05:15 +0800

    首次测试

---
+++ convert_list_to_m3u.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+import json
+
+
+if __name__ == "__main__":
+
+    print("正在转换频道列表...")
+    with open('channellist.json', 'r', encoding='utf8') as fcc_file:
+        fcc_data = json.load(fcc_file)
+
+        # items = fcc_data.items()
+
+        with open("gd_unicom_iptv.m3u", "w",encoding="utf8") as file:
+            file.write("#EXTM3U\n")
+            for item in fcc_data:
+                id = item["channelID"]
+                name = item["channelName"]
+                url = item["channelURL"][0]
+                group_name = ""
+                if("CCTV" in name):
+                    group_name = "央视"
+                if("卫视" in name):
+                    group_name = "卫视"
+                print("正在生成频道: {}".format(name))
+
+                file.write(
+                    "#EXTINF:-1 tvg-id=\"{}\" tvg-name=\"{}\"  group-title=\"{}\",{}\n".format(name, name, group_name, name))
+                file.write("{}\n".format(url))
