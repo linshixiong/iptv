74d920dd1537d419daeb130ac457b3d9ba4192ad
linshixiong <linsx.lin@emdoor.com>
2023-10-10 11:31:03 +0800

    优化台标显示

--- convert_list_to_m3u.py
+++ convert_list_to_m3u.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import json
-
+import re
 
 if __name__ == "__main__":
 
@@ -10,26 +10,30 @@ if __name__ == "__main__":
 
         # items = fcc_data.items()
 
-        with open("gd_unicom_iptv.m3u", "w",encoding="utf8") as file:
+        with open("gd_unicom_iptv.m3u", "w", encoding="utf8") as file:
             file.write("#EXTM3U\n")
             for item in fcc_data:
                 id = item["channelID"]
                 name = item["channelName"]
                 url = item["channelURL"][0]
                 group_name = "未分组"
-                short_name = name.replace("高清1","").replace("标清1","").replace("标清","").replace("高清","")
-                if("CCTV" in name or "央视" in name):
+                short_name = name.replace("高清1", "").replace("标清1", "").replace("标清", "").replace(
+                    "高清", "").replace("超清", "").replace("-", "").replace("测试", "")
+                if ("CCTV" in name):
                     group_name = "央视"
-                if("卫视" in name):
+                    short_name = re.sub(u"([^\u0041-\u005a\u0061-\u007a\u0030-\u0039])", "", short_name)
+                if ("央视" in name):
+                    group_name = "央视"
+                if ("卫视" in name):
                     group_name = "卫视"
-                if("CETV" in name):
+                if ("CETV" in name):
                     group_name = "CETV"
-                if("广东" in name or "深圳" in name):
+                if ("广东" in name or "深圳" in name):
                     group_name = "广东"
-                    
-                if("高清" in name):
-                    group_name += ";高清"    
-            
+
+                if ("高清" in name):
+                    group_name += ";高清"
+
                 print("正在生成频道: {}".format(name))
 
                 file.write(
